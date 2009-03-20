872b8dc742ab19f60da00bb8774c254eab72511c
albumasar <albumasar@29f3e0cf-a44d-0410-bd1c-a6599a98236d>
2009-03-20 14:29:38 +0000

    support for sets; call parsename within _parse_text_bibtex

--- lib/Biber/BibTeX.pm
+++ lib/Biber/BibTeX.pm
@@ -1,71 +1,100 @@
-package Biber::BibTeX;
-use strict;
-use warnings;
-use Carp;
-use Text::BibTeX;
-use Biber::Constants;
-use Biber::Utils;
-use Encode;
+package Biber::BibTeX ;
+use strict ;
+use warnings ;
+use Carp ;
+use Text::BibTeX ;
+use Biber::Constants ;
+use Biber::Utils ;
+use Encode ;
 
 sub _text_bibtex_parse {
-    
-    my ($self, $filename) = @_;
+        
+    my ($self, $filename) = @_ ;
 
-    my @auxcitekeys = $self->citekeys;
+    my %bibentries = $self->bib ;
     
-    my %bibentries = $self->bib;
-    
-    my @localkeys;
+    my @localkeys ;
 
     my $bib = new Text::BibTeX::File $filename
-     or croak "Cannot create Text::BibTeX::File object from $filename: $!";
+     or croak "Cannot create Text::BibTeX::File object from $filename: $!" ;
 
     #TODO validate with Text::BibTeX::Structure ?
-    my $preamble;
+    my $preamble ;
 
     while ( my $entry = new Text::BibTeX::Entry $bib ) {
 
-       next if ( $entry->metatype == BTE_MACRODEF ) ;                    
+        next if ( $entry->metatype == BTE_MACRODEF ) ;                    
 
-       my $key = $entry->key ;
+        my $key = $entry->key ;
 
-       print "Processing $key\n" if $self->config('biberdebug');
+        print "Processing $key\n" if $self->config('biberdebug') ;
 
-       if ( $bibentries{ $key } ) {
-           print "We already have key $key! Ignoring in $filename...\n"
-               unless $self->config('quiet');
-           next;
-       }
-       push @localkeys, $key;
-       unless ($entry->parse_ok) {
-           carp "Entry $key does not parse correctly: skipping" 
-               unless $self->config('quiet') ;
-           next ;
-       }
-       if ( $entry->metatype == BTE_PREAMBLE ) {
-           $preamble .= $entry->value;
-   		next;
-       }
+        if ( $bibentries{ $key } ) {
+            print "We already have key $key! Ignoring in $filename...\n"
+                unless $self->config('quiet') ;
+            next ;
+        }
+        push @localkeys, $key ;
+        unless ($entry->parse_ok) {
+            carp "Entry $key does not parse correctly: skipping" 
+                unless $self->config('quiet') ;
+            next ;
+        }
+        if ( $entry->metatype == BTE_PREAMBLE ) {
+            $preamble .= $entry->value ;
+        next ;
+        }
 
-    my @flist = $entry->fieldlist ;
+        my @flist = $entry->fieldlist ;
 
-    my @flistnosplit = array_minus(\@flist, \@ENTRIESTOSPLIT);
+        my @flistnosplit = array_minus(\@flist, \@ENTRIESTOSPLIT) ;
 
-   	if ( $entry->metatype == BTE_REGULAR ) {
-           foreach my $f ( @flistnosplit ) {
-               $bibentries{ $key }->{$f} =
-                 decode_utf8( $entry->get($f) );
-           };
-   		foreach my $f ( @ENTRIESTOSPLIT ) {
-   			next unless $entry->exists($f);
-   			my @tmp = map { decode_utf8($_) } $entry->split($f);
-   			$bibentries{ $key }->{$f} = [ @tmp ] 
-   		};
+        if ( $entry->metatype == BTE_REGULAR ) {
 
-           $bibentries{ $key }->{entrytype} = $entry->type;
-           $bibentries{ $key }->{datatype} = 'bibtex';
-       }
-   }
+            foreach my $f ( @flistnosplit ) {
+
+                my $value = decode_utf8( $entry->get($f) ) ;
+
+                $bibentries{ $key }->{$f} = $value ;
+
+                if ($entry->type eq 'set' and $f eq 'entryset') {
+                    
+                    my @entrysetkeys = split /\s*,\s*/, $value ; 
+
+                    foreach my $setkey (@entrysetkeys) {
+                        $Biber::inset_entries{$setkey} = $key ;
+                    }
+                }
+                elsif ($f eq 'crossref') { ### $entry->type ne 'set' and 
+                    $Biber::crossrefkeys{$value}++ ;
+                    $Biber::entrieswithcrossref{$key} = $value ;                   
+                }
+            } ;
+
+            foreach my $f ( @ENTRIESTOSPLIT ) {
+
+                next unless $entry->exists($f) ;
+
+                my @tmp = map { decode_utf8($_) } $entry->split($f) ;
+
+                if ($Biber::is_name_entry{$f}) {
+
+                    # this returns an array of hashes
+                    @tmp = map { $self->parsename( $_ , $key) } @tmp ;
+
+                } ;
+                    
+                $bibentries{ $key }->{$f} = [ @tmp ]                 
+
+            } ;
+
+            $bibentries{ $key }->{entrytype} = $entry->type ;
+
+            $bibentries{ $key }->{datatype} = 'bibtex' ;
+       
+        }
+
+    }
 
    $self->{bib} = { %bibentries } ;
 
@@ -73,7 +102,7 @@ sub _text_bibtex_parse {
 
 }
 
-1;
+1 ;
 
 __END__
 
@@ -105,3 +134,4 @@ under the same terms as Perl itself.
 
 =cut
 
+# vim: set tabstop=4 shiftwidth=4: 
