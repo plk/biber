b8c7dd72a6fa3299d757ed4eeaf002f117a9cab4
François Charette <albumasar@users.sourceforge.net>
2009-06-09 10:48:22 +0200

    Added missing field "foreword" in Biber::Constants

--- lib/Biber/Constants.pm
+++ lib/Biber/Constants.pm
@@ -45,9 +45,9 @@ our %CONFIG_DEFAULT = (
 ### biblatex fields
 
 Readonly::Array our @NAMEFIELDS  =>   qw{
-  author editor shortauthor shorteditor commentator 
-  translator redactor bookauthor afterword introduction 
-  holder sortname namea nameb namec } ;
+  author editor shortauthor shorteditor commentator translator redactor 
+  bookauthor introduction foreword afterword holder sortname 
+  namea nameb namec } ;
 
 Readonly::Array our @LISTFIELDS  =>   qw{
   publisher location address institution school language origlocation
