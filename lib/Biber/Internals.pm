6c173ef9233739f2c2f73d19624b3f27b6e73e0f
François Charette <albumasar@users.sourceforge.net>
2009-06-10 17:15:49 +0200

    Update authors and license info

--- lib/Biber/Internals.pm
+++ lib/Biber/Internals.pm
@@ -778,6 +778,7 @@ sub _print_biblatex_entry {
 =head1 AUTHOR
 
 François Charette, C<< <firmicus at gmx.net> >>
+Philip Kime C<< <philip at kime.org.uk> >>
 
 =head1 BUGS
 
@@ -786,7 +787,7 @@ L<https://sourceforge.net/tracker2/?func=browse&group_id=228270>.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 François Charette, all rights reserved.
+Copyright 2009 François Charette and Philip Kime, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
--- lib/Biber/Utils.pm
+++ lib/Biber/Utils.pm
@@ -460,6 +460,7 @@ sub ucinit {
 =head1 AUTHOR
 
 François Charette, C<< <firmicus at gmx.net> >>
+Philip Kime C<< <philip at kime.org.uk> >>
 
 =head1 BUGS
 
@@ -468,7 +469,7 @@ L<https://sourceforge.net/tracker2/?func=browse&group_id=228270>.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 François Charette, all rights reserved.
+Copyright 2009 François Charette and Philip Kime, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
