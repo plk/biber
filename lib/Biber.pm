7dca2062d2bac9150e24fa560ec0ab26b31e922e
François Charette <albumasar@users.sourceforge.net>
2010-02-15 22:44:44 +0100

    bump to v0.5.1

--- lib/Biber.pm
+++ lib/Biber.pm
@@ -32,11 +32,11 @@ Biber - main module for biber, a bibtex replacement for users of biblatex
 
 =head1 VERSION
 
-Version 0.5
+Version 0.5.1
 
 =cut
 
-our $VERSION = '0.5';
+our $VERSION = '0.5.1';
 
 =head1 SYNOPSIS
 
