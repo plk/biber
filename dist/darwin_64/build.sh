b47f9f37f79ff0bde0e4c8a29be3f9d4efddd020
Philip Kime <Philip@kime.org.uk>
2010-09-09 17:10:37 +0200

    Added libbtparse dep

--- dist/darwin_64/build.sh
+++ dist/darwin_64/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pp --compress=0 --link=/opt/local/lib/libxml2.2.dylib --addlist=biber.files --cachedeps=scancache --output=biber /opt/local/bin/biber
+pp --compress=0 --link=/opt/local/lib/libbtparse.dylib --link=/opt/local/lib/libxml2.2.dylib --addlist=biber.files --cachedeps=scancache --output=biber /opt/local/bin/biber
