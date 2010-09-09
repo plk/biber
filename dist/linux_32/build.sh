5a0311c3982c7796a997efc85465333f98fe9375
Philip Kime <Philip@kime.org.uk>
2010-09-09 17:18:00 +0200

    libxml path on linux

--- dist/linux_32/build.sh
+++ dist/linux_32/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pp --compress=0 --link=/usr/local/lib/libbtparse.so --link=/usr/local/lib/libxml2.so.2 --addlist=biber.files --cachedeps=scancache --output=biber /usr/local/bin/biber
+pp --compress=0 --link=/usr/local/lib/libbtparse.so --link=/usr/lib/libxml2.so.2 --addlist=biber.files --cachedeps=scancache --output=biber /usr/local/bin/biber
