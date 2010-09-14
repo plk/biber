00381c57c3323cc50ddee84c838a5142fd8bf0e4
Philip Kime <Philip@kime.org.uk>
2010-09-14 18:33:10 +0200

    Added more deps to linux builds

--- dist/linux_x86_64/build.sh
+++ dist/linux_x86_64/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pp --compress=6 --link=/usr/local/lib/libbtparse.so --link=/usr/lib/libxml2.so.2 --addlist=biber.files --cachedeps=scancache --output=biber-linux_x86_64 /usr/local/bin/biber
+pp --compress=6 --link=/lib/libdl.so.2 --link=/lib/libc.so.6 --link=/lib64/ld-linux-x86-64.so.2 --link=/lib/libm.so.6 --link=/lib/libpthread.so.0 --link=/lib/libcrypt.so.1  --link=/usr/local/lib/libbtparse.so --link=/usr/lib/libxml2.so.2 --addlist=biber.files --cachedeps=scancache --output=biber-linux_x86_64 /usr/local/bin/biber
