252baa4459cfe765bdde5e5075de624ccdd097b2
Philip Kime <Philip@kime.org.uk>
2010-09-17 21:42:50 +0200

    upper_before_lower is now default for U::C sort

--- lib/Biber/Constants.pm
+++ lib/Biber/Constants.pm
@@ -80,7 +80,7 @@ our %CONFIG_DEFAULT_BIBER = (
   wraplines => 0,
   #
   # these options are passed to the Unicode::Collate object
-  collate_options => { level=>2 },
+  collate_options => { level => 2, upper_before_lower => 1 },
   #
   # eventually this shall be moved to biblatex options:
   displaymode => $DISPLAYMODE_DEFAULT,
