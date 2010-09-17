fe8722199afadb9f239b4442b68ce4e67647ed70
Philip Kime <Philip@kime.org.uk>
2010-09-17 20:02:11 +0200

    Binaries need explicit Encode::* modules

--- dist/darwin_x86_64/build.sh
+++ dist/darwin_x86_64/build.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-pp --compress=6 --link=/opt/local/lib/libz.1.dylib --link=/opt/local/lib/libiconv.2.dylib --link=/opt/local/lib/libbtparse.dylib --link=/opt/local/lib/libxml2.2.dylib --addlist=biber.files --cachedeps=scancache --output=biber-darwin_x86_64 /opt/local/bin/biber
+pp --compress=6 \
+  --module=Encode::Byte \
+  --module=Encode::CN \
+  --module=Encode::CJKConstants \
+  --module=Encode::EBCDIC \
+  --module=Encode::Encoder \
+  --module=Encode::GSM0338 \
+  --module=Encode::Guess \
+  --module=Encode::JP \
+  --module=Encode::KR \
+  --module=Encode::MIME::Header \
+  --module=Encode::Symbol \
+  --module=Encode::TW \
+  --module=Encode::Unicode \
+  --module=Encode::Unicode::UTF7 \
+  --link=/opt/local/lib/libz.1.dylib \
+  --link=/opt/local/lib/libiconv.2.dylib \
+  --link=/opt/local/lib/libbtparse.dylib \
+  --link=/opt/local/lib/libxml2.2.dylib \
+  --addlist=biber.files \
+  --cachedeps=scancache \
+  --output=biber-darwin_x86_64 \
+  /opt/local/bin/biber
 
--- dist/linux_x86_32/build.sh
+++ dist/linux_x86_32/build.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-/usr/local/perl/bin/pp --compress=6 --link=/usr/local/perl/lib/libbtparse.so --link=/usr/lib/libxml2.so.2 --addlist=biber.files --cachedeps=scancache --output=biber-linux_x86_32 /usr/local/perl/bin/biber
+/usr/local/perl/bin/pp \
+  --compress=6 \
+  --module=Encode::Byte \
+  --module=Encode::CN \
+  --module=Encode::CJKConstants \
+  --module=Encode::EBCDIC \
+  --module=Encode::Encoder \
+  --module=Encode::GSM0338 \
+  --module=Encode::Guess \
+  --module=Encode::JP \
+  --module=Encode::KR \
+  --module=Encode::MIME::Header \
+  --module=Encode::Symbol \
+  --module=Encode::TW \
+  --module=Encode::Unicode \
+  --module=Encode::Unicode::UTF7 \
+  --link=/usr/local/perl/lib/libbtparse.so \
+  --link=/usr/lib/libxml2.so.2 \
+  --addlist=biber.files \
+  --cachedeps=scancache \
+  --output=biber-linux_x86_32 \
+  /usr/local/perl/bin/biber
