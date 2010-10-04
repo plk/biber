fc1ac4aeb9467fb684555c58930f21e95ead4e0a
Philip Kime <Philip@kime.org.uk>
2010-10-04 18:55:14 +0200

    Modified binary upload script

--- dist/uploadsf.sh
+++ dist/uploadsf.sh
@@ -6,12 +6,16 @@
 # biber-linux_x86_32
 # biber-linux_x86_64
 
+DIR=${1:-"~/Desktop/b"}
+RELEASE=${2:-"development"}
+
+
 # Windows
-scp $1/biber-MSWIN.exe philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/development/binaries/Windows/biber.exe
+scp $DIR/biber-MSWIN.exe philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Windows/biber.exe
 # OSX
-scp $1/biber-darwin_x86_64 philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/development/binaries/OSX_Intel/biber
+scp $DIR/biber-darwin_x86_64 philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/OSX_Intel/biber
 # Linux 32-bit
-scp $1/biber-linux_x86_32 philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/development/binaries/Linux_32bit/biber
+scp $DIR/biber-linux_x86_32 philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Linux_32bit/biber
 # Linux 64-bit
-scp $1/biber-linux_x86_64 philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/development/binaries/Linux_64bit/biber
+scp $DIR/biber-linux_x86_64 philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Linux_64bit/biber
 
