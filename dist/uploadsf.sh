4ffec449c0f5fa72a0c66713822718b18ad0189a
Philip Kime <Philip@kime.org.uk>
2010-10-26 11:28:32 +0200

    Fixed dist script

--- dist/uploadsf.sh
+++ dist/uploadsf.sh
@@ -9,27 +9,28 @@
 DIR=${1:-"/Users/philkime/Desktop/b"}
 RELEASE=${2:-"development"}
 
-
+cd $DIR
 # Windows
-mv $DIR/biber-MSWIN.exe $DIR/biber.exe
-/usr/bin/zip $DIR/biber.zip $DIR/biber.exe
-scp $DIR/biber.zip philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Windows/biber.zip
-\rm $DIR/biber.zip $DIR/biber.exe
+mv biber-MSWIN.exe biber.exe
+/usr/bin/zip biber.zip biber.exe
+scp biber.zip philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Windows/biber.zip
+\rm biber.zip biber.exe
 # OSX
-mv $DIR/biber-darwin_x86_64 $DIR/biber
-tar cf $DIR/biber.tar $DIR/biber
-gzip $DIR/biber.tar
-scp $DIR/biber.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/OSX_Intel/biber.tar.gz
-\rm $DIR/biber.tar.gz $DIR/biber
+mv biber-darwin_x86_64 biber
+tar cf biber.tar biber
+gzip biber.tar
+scp biber.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/OSX_Intel/biber.tar.gz
+\rm $DIR/biber.tar.gz biber
 # Linux 32-bit
-mv $DIR/biber-linux_x86_32 $DIR/biber
-tar cf $DIR/biber.tar $DIR/biber
-gzip $DIR/biber.tar
-scp $DIR/biber.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Linux_32bit/biber.tar.gz
-\rm $DIR/biber.tar.gz $DIR/biber
+mv biber-linux_x86_32 biber
+tar cf biber.tar biber
+gzip biber.tar
+scp biber.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Linux_32bit/biber.tar.gz
+\rm biber.tar.gz biber
 # Linux 64-bit
-mv $DIR/biber-linux_x86_64 $DIR/biber
-tar cf $DIR/biber.tar $DIR/biber
-gzip $DIR/biber.tar
-scp $DIR/biber.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Linux_64bit/biber.tar.gz
-\rm $DIR/biber.tar.gz $DIR/biber
+mv biber-linux_x86_64 biber
+tar cf biber.tar biber
+gzip biber.tar
+scp biber.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Linux_64bit/biber.tar.gz
+\rm biber.tar.gz biber
+
