c0d69939a63e0a7381a99c2acb83873d651659a1
Philip Kime <Philip@kime.org.uk>
2010-10-27 11:26:27 +0200

    typo in dist script

--- dist/uploadsf.sh
+++ dist/uploadsf.sh
@@ -15,7 +15,7 @@ mv biber-MSWIN.exe biber.exe
 chmod +x biber.exe
 /usr/bin/zip biber.zip biber.exe
 scp biber.zip philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Windows/biber.zip
-\rm biber.exe
+\rm biber.zip
 # OSX
 mv biber-darwin_x86_64 biber
 chmod +x biber
