7bc54fc72519a9573c38871594771d36336a7fa9
Philip Kime <Philip@kime.org.uk>
2010-10-29 19:11:41 +0200

    Updated dist script again

--- dist/uploadsf.sh
+++ dist/uploadsf.sh
@@ -11,30 +11,30 @@ RELEASE=${2:-"development"}
 
 cd $DIR
 # Windows
-mv biber-MSWIN.exe biber.exe
+cp biber-MSWIN.exe biber.exe
 chmod +x biber.exe
 /usr/bin/zip biber.zip biber.exe
 scp biber.zip philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Windows/biber.zip
-\rm biber.zip
+\rm biber.zip biber.exe
 # OSX
-mv biber-darwin_x86_64 biber
+cp biber-darwin_x86_64 biber
 chmod +x biber
 tar cf biber.tar biber
 gzip biber.tar
 scp biber.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/OSX_Intel/biber.tar.gz
-\rm biber.tar.gz
+\rm biber.tar.gz biber
 # Linux 32-bit
-mv biber-linux_x86_32 biber
+cp biber-linux_x86_32 biber
 chmod +x biber
 tar cf biber.tar biber
 gzip biber.tar
 scp biber.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Linux_32bit/biber.tar.gz
-\rm biber.tar.gz
+\rm biber.tar.gz biber
 # Linux 64-bit
-mv biber-linux_x86_64 biber
+cp biber-linux_x86_64 biber
 chmod +x biber
 tar cf biber.tar biber
 gzip biber.tar
 scp biber.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Linux_64bit/biber.tar.gz
-\rm biber.tar.gz
+\rm biber.tar.gz biber
 
