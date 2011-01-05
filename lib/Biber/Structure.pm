0a0fcf94c1396168c85ecd6beeaf1b80ed7c1e76
Philip Kime <Philip@kime.org.uk>
2011-01-05 11:57:59 +0100

    Updated copyright years

--- lib/Biber/Structure.pm
+++ lib/Biber/Structure.pm
@@ -565,5 +565,28 @@ sub dump {
   return pp($self);
 }
 
+=head1 AUTHORS
+
+François Charette, C<< <firmicus at gmx.net> >>
+Philip Kime C<< <philip at kime.org.uk> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests on our sourceforge tracker at
+L<https://sourceforge.net/tracker2/?func=browse&group_id=228270>.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009-2011 François Charette and Philip Kime, all rights reserved.
+
+This module is free software.  You can redistribute it and/or
+modify it under the terms of the Artistic License 2.0.
+
+This program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
+
+=cut
+
 
 1;
