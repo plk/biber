63fd0c32deb2ecfe79e93b921dec7fe6c703b795
Philip Kime <Philip@kime.org.uk>
2011-01-06 15:26:46 +0100

    Bumped version for dev

--- lib/Biber.pm
+++ lib/Biber.pm
@@ -38,8 +38,8 @@ Biber - main module for biber, a bibtex replacement for users of biblatex
 
 =cut
 
-our $VERSION = '0.7';
-our $BETA_VERSION = 0; # Is this a beta version?
+our $VERSION = '0.7.1';
+our $BETA_VERSION = 1; # Is this a beta version?
 
 =head1 SYNOPSIS
 
