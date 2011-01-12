a19e5d376bb6d0eed9334a9b344bccef61456dfe
Philip Kime <Philip@kime.org.uk>
2011-01-12 20:45:36 +0100

    Another small ctrl filoe fix

--- lib/Biber.pm
+++ lib/Biber.pm
@@ -190,7 +190,7 @@ sub parse_ctrlfile {
   my $ctrl_file_path = locate_biber_file($ctrl_file);
   Biber::Config->set_ctrlfile_path($ctrl_file_path);
 
-  $logger->logcroak("Cannot find control file '$ctrl_file'! - did you pass the \"backend=biber\" option to BibLaTeX?") unless -f "$ctrl_file";
+  $logger->logcroak("Cannot find control file '$ctrl_file'! - did you pass the \"backend=biber\" option to BibLaTeX?") unless ($ctrl_file_path and -e $ctrl_file_path);
 
   # Validate if asked to
   if (Biber::Config->getoption('validate_control')) {
