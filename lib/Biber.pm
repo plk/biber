1ca84867c7f7481597e0d5fb3b5df61f17bbf302
Philip Kime <Philip@kime.org.uk>
2011-02-03 17:41:18 +0100

    Prep for 0.7.3 release

--- lib/Biber.pm
+++ lib/Biber.pm
@@ -40,7 +40,7 @@ Biber - main module for biber, a bibtex replacement for users of biblatex
 =cut
 
 our $VERSION = '0.7.3';
-our $BETA_VERSION = 1; # Is this a beta version?
+our $BETA_VERSION = 0; # Is this a beta version?
 
 my $logger = Log::Log4perl::get_logger('main');
 
