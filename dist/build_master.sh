3fba5bd055ede6c44b2692da555623ac0e7612c8
Philip Kime <Philip@kime.org.uk>
2011-03-10 19:23:03 +0100

    Updated build script for cygwin

--- dist/build_master.sh
+++ dist/build_master.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# uploadsf.sh <dir> <release>
+# build_master.sh <dir> <release> <branch> <justbuild>
 
-# where <dir> is where these are:
+# <dir> is where these are:
 # biber-MSWIN.exe
+# biber-cygwin32
 # biber-darwin_x86
 # biber-linux_x86_32
 # biber-linux_x86_64
 
-# and <release> is a subdir of /home/frs/project/b/bi/biblatex-biber/biblatex-biber/
-# on SF
+# <release> is a SF subdir of /home/frs/project/b/bi/biblatex-biber/biblatex-biber/
+# <branch> is a git branch to checkout on the build farm servers
+# <justbuild> is a boolean which says to just build and stop without uploading
+
+# Example: build_master.sh ~/Desktop/b development dev 1
 
 BASE="/Users/philkime/data/code/biblatex-biber"
 DOCDIR=$BASE/doc
@@ -18,7 +22,13 @@ BINDIR=$BASE/dist
 XSLDIR=$BASE/data
 DIR=${1:-"/Users/philkime/Desktop/b"}
 RELEASE=${2:-"development"}
-export COPYFILE_DISABLE=true # no resource forks - TL doesn't like them
+BRANCH=${3:-"dev"}
+JUSTBUILD=${4:-"0"}
+export COPYFILE_DISABLE=true # no resource forks - TLContrib upload doesn't like them
+
+echo "** Checking out branch '$BRANCH' on farm servers **"
+echo "** If this is not correct, Ctrl-C now **"
+sleep 5
 
 # Make binary dir if it doesn't exist
 if [ ! -e $DIR ]; then
@@ -29,8 +39,8 @@ fi
 # Build farm OSX universal 32/64 intel
 if [ ! -e $DIR/biber-darwin_x86 ]; then
   ssh root@wood "VBoxHeadless --startvm bbf-osx32 </dev/null >/dev/null 2>&1 &"
-  sleep 5
-  ssh bbf-osx32 "cd biblatex-biber;git pull;perl ./Build.PL;sudo ./Build install;cd dist/darwin_x86;./build.sh"
+  sleep 4
+  ssh bbf-osx32 "cd biblatex-biber;git checkout $BRANCH;git pull;perl ./Build.PL;sudo ./Build install;cd dist/darwin_x86;\\rm -f biber-darwin_x86;./build.sh"
   scp bbf-osx32:biblatex-biber/dist/darwin_x86/biber-darwin_x86 $DIR/
   ssh root@wood "VBoxManage controlvm bbf-osx32 savestate"
 fi
@@ -38,8 +48,8 @@ fi
 # Build farm WinXP
 if [ ! -e $DIR/biber-MSWIN.exe ]; then
   ssh root@wood "VBoxHeadless --startvm bbf-wxp32 </dev/null >/dev/null 2>&1 &"
-  sleep 5
-  ssh bbf-wxp32 "cd biblatex-biber;git pull;perl ./Build.PL;./Build install;cd dist/MSWin32;./build.bat"
+  sleep 4
+  ssh bbf-wxp32 "cd biblatex-biber;git checkout $BRANCH;git pull;perl ./Build.PL;./Build install;cd dist/MSWin32;\\rm -f biber-MSWIN.exe;./build.bat"
   scp bbf-wxp32:biblatex-biber/dist/MSWin32/biber-MSWIN.exe $DIR/
   ssh root@wood "VBoxManage controlvm bbf-wxp32 savestate"
 fi
@@ -47,9 +57,9 @@ fi
 # Build farm cygwin
 if [ ! -e $DIR/biber-cygwin32 ]; then
   ssh root@wood "VBoxHeadless --startvm bbf-wxp32 </dev/null >/dev/null 2>&1 &"
-  sleep 5
+  sleep 4
   # We have to move aside the windows libbtparse.dll otherwise it's picked up by cygwin
-  ssh bbf-wxp32 ". bin/set-biber-cyg-build-env.sh;mv /cygdrive/c/WINDOWS/libbtparse.dll /cygdrive/c/WINDOWS/libbtparse.dll.DIS;cd biblatex-biber;git pull;perl ./Build.PL;./Build install;cd dist/cygwin32;./build.sh;mv /cygdrive/c/WINDOWS/libbtparse.dll.DIS /cygdrive/c/WINDOWS/libbtparse.dll"
+  ssh bbf-wxp32 ". bin/set-biber-cyg-build-env.sh;mv /cygdrive/c/WINDOWS/libbtparse.dll /cygdrive/c/WINDOWS/libbtparse.dll.DIS;cd biblatex-biber;git checkout $BRANCH;git pull;perl ./Build.PL;./Build install;cd dist/cygwin32;\\rm -f biber-cygwin32;./build.sh;mv /cygdrive/c/WINDOWS/libbtparse.dll.DIS /cygdrive/c/WINDOWS/libbtparse.dll"
   scp bbf-wxp32:biblatex-biber/dist/cygwin32/biber-cygwin32 $DIR/
   ssh root@wood "VBoxManage controlvm bbf-wxp32 savestate"
 fi
@@ -57,8 +67,8 @@ fi
 # Build farm Linux 32
 if [ ! -e $DIR/biber-linux_x86_32 ]; then
   ssh root@wood "VBoxHeadless --startvm bbf-jj32 </dev/null >/dev/null 2>&1 &"
-  sleep 5
-  ssh bbf-jj32 "cd biblatex-biber;git pull;/usr/local/perl/bin/perl ./Build.PL;sudo ./Build install;cd dist/linux_x86_32;./build.sh"
+  sleep 4
+  ssh bbf-jj32 "cd biblatex-biber;git checkout $BRANCH;git pull;/usr/local/perl/bin/perl ./Build.PL;sudo ./Build install;cd dist/linux_x86_32;\\rm -f biber-linux_x86_32;./build.sh"
   scp bbf-jj32:biblatex-biber/dist/linux_x86_32/biber-linux_x86_32 $DIR/
   ssh root@wood "VBoxManage controlvm bbf-jj32 savestate"
 fi
@@ -66,10 +76,16 @@ fi
 # Build farm Linux 64
 if [ ! -e $DIR/biber-linux_x86_64 ]; then
   ssh root@wood "VBoxHeadless --startvm bbf-jj64 </dev/null >/dev/null 2>&1 &"
-  sleep 5
-  ssh bbf-jj64 "cd biblatex-biber;git pull;/usr/local/perl/bin/perl ./Build.PL;sudo ./Build install;cd dist/linux_x86_64;./build.sh"
+  sleep 4
+  ssh bbf-jj64 "cd biblatex-biber;git checkout $BRANCH;git pull;/usr/local/perl/bin/perl ./Build.PL;sudo ./Build install;cd dist/linux_x86_64;\\rm -f biber-linux_x86_64;./build.sh"
   scp bbf-jj64:biblatex-biber/dist/linux_x86_64/biber-linux_x86_64 $DIR/
   ssh root@wood "VBoxManage controlvm bbf-jj64 savestate"
+fi
+
+# Stop here if JUSTBUILD is set
+if [ "$JUSTBUILD" = "1" ]; then
+  echo "JUSTBUILD is set, won't upload anything";
+  exit 0;
 fi
 
 cd $DIR
@@ -79,6 +95,13 @@ chmod +x biber.exe
 /usr/bin/zip biber-MSWIN.zip biber.exe
 scp biber-MSWIN.zip philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Windows/biber-MSWIN.zip
 \rm biber-MSWIN.zip biber.exe
+# cygwin
+cp biber-cygwin32 biber
+chmod +x biber
+tar cf biber-cygwin32.tar biber
+gzip biber-cygwin32.tar
+scp biber-cygwin32.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/binaries/Cygwin/biber-cygwin32.tar.gz
+\rm biber-cygwin32.tar.gz biber
 # OSX
 cp biber-darwin_x86 biber
 chmod +x biber
@@ -105,8 +128,8 @@ scp $DOCDIR/biber.pdf philkime,biblatex-biber@frs.sourceforge.net:/home/frs/proj
 # Changes file
 scp $BASE/Changes philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/Changes
 # Driver control file docs
-find $DRIVERDIR -name \*.dcf | xargs -I{} cp {} ~/Desktop/
-for dcf in ~/Desktop/*.dcf
+find $DRIVERDIR -name \*.dcf | xargs -I{} cp {} $DIR
+for dcf in $DIR/*.dcf
 do
 $BINDIR/make-pretty-dcfs.pl $dcf $XSLDIR/dcf.xsl
 scp $dcf.html philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/b/bi/biblatex-biber/biblatex-biber/$RELEASE/documentation/drivers/
