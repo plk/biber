5fdd824309d8c5ceebf0ffeed90fd3f2557828e4
Philip Kime <Philip@kime.org.uk>
2011-09-19 21:18:08 +0200

    cygwin to 5.14.1

--- dist/cygwin32/build.sh
+++ dist/cygwin32/build.sh
@@ -11,43 +11,42 @@
 # Have to explicitly include the Input* modules as the names of these are dynamically
 # constructed in the code so Par::Packer can't auto-detect them
 
-#  --module=deprecate \ # add this back in when cygwin has perl 5.12+
-
 cp /usr/local/bin/biber /tmp/biber-cygwin
 
 pp --compress=6 \
-  --module=Biber::Input::file::bibtex \
-  --module=Biber::Input::file::biblatexml \
-  --module=Biber::Input::file::ris \
-  --module=Biber::Input::file::zoterordfxml \
-  --module=Biber::Input::file::endnotexml \
-  --module=Pod::Simple::TranscodeSmart \
-  --module=Pod::Simple::TranscodeDumb \
-  --module=Encode::Byte \
-  --module=Encode::CN \
-  --module=Encode::CJKConstants \
-  --module=Encode::EBCDIC \
-  --module=Encode::Encoder \
-  --module=Encode::GSM0338 \
-  --module=Encode::Guess \
-  --module=Encode::JP \
-  --module=Encode::KR \
-  --module=Encode::MIME::Header \
-  --module=Encode::Symbol \
-  --module=Encode::TW \
-  --module=Encode::Unicode \
-  --module=Encode::Unicode::UTF7 \
-  --link=/usr/bin/cygz.dll \
-  --link=/usr/bin/cyggcrypt-11.dll \
-  --link=/usr/bin/cygiconv-2.dll \
-  --link=/usr/bin/cyggpg-error-0.dll \
-  --link=/usr/local/bin/libbtparse.dll \
-  --link=/usr/bin/cygxml2-2.dll \
-  --link=/usr/bin/cygxslt-1.dll \
-  --link=/usr/bin/cygexslt-0.dll \
-  --addlist=biber.files \
-  --cachedeps=scancache \
-  --output=biber-cygwin32.exe \
-  /tmp/biber-cygwin
+   --module=deprecate \
+   --module=Biber::Input::file::bibtex \
+   --module=Biber::Input::file::biblatexml \
+   --module=Biber::Input::file::ris \
+   --module=Biber::Input::file::zoterordfxml \
+   --module=Biber::Input::file::endnotexml \
+   --module=Pod::Simple::TranscodeSmart \
+   --module=Pod::Simple::TranscodeDumb \
+   --module=Encode::Byte \
+   --module=Encode::CN \
+   --module=Encode::CJKConstants \
+   --module=Encode::EBCDIC \
+   --module=Encode::Encoder \
+   --module=Encode::GSM0338 \
+   --module=Encode::Guess \
+   --module=Encode::JP \
+   --module=Encode::KR \
+   --module=Encode::MIME::Header \
+   --module=Encode::Symbol \
+   --module=Encode::TW \
+   --module=Encode::Unicode \
+   --module=Encode::Unicode::UTF7 \
+   --link=/usr/bin/cygz.dll \
+   --link=/usr/bin/cyggcrypt-11.dll \
+   --link=/usr/bin/cygiconv-2.dll \
+   --link=/usr/bin/cyggpg-error-0.dll \
+   --link=/usr/local/bin/libbtparse.dll \
+   --link=/usr/bin/cygxml2-2.dll \
+   --link=/usr/bin/cygxslt-1.dll \
+   --link=/usr/bin/cygexslt-0.dll \
+   --addlist=biber.files \
+   --cachedeps=scancache \
+   --output=biber-cygwin32.exe \
+   /tmp/biber-cygwin
 
 \rm -f /tmp/biber-cygwin
