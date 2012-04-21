b022c26661cf120add555dbaeb0eed0272890783
Philip Kime <Philip@kime.org.uk>
2012-04-21 11:33:46 +0200

    Revert linux 64 build script for now, seems to make no difference

--- dist/linux_x86_64/build.sh
+++ dist/linux_x86_64/build.sh
@@ -13,16 +13,6 @@
 # Added libz as some linux distros like SUSE 11.3 have a slightly older zlib
 # which doesn't have gzopen64 in it.
 
-# Also, pp resolves symlinks and copies the symlink targets of linked libs
-# which then don't have the right names and so things that link to them
-# through the link name break. So, we copy them to the link names first and
-# and package those.
-
-cp /lib/libz.so.1.2.3.3 /tmp/libz.so.1
-cp /usr/lib/libxml2.so.2.6.32 /tmp/libxml2.so.2
-cp /usr/lib/libxslt.so.1.1.24 /tmp/libxslt.so.1
-cp /usr/lib/libexslt.so.0.8.13 /tmp/libexslt.so.0
-
 /usr/local/perl/bin/pp \
   --compress=6 \
   --module=deprecate \
@@ -50,18 +40,13 @@ cp /usr/lib/libexslt.so.0.8.13 /tmp/libexslt.so.0
   --module=Readonly::XS \
   --module=IO::Socket::SSL \
   --link=/usr/local/perl/lib64/libbtparse.so \
-  --link=/tmp/libxml2.so.2 \
-  --link=/tmp/libxslt.so.1 \
-  --link=/tmp/libexslt.so.0 \
-  --link=/tmp/libz.so.1 \
+  --link=/usr/lib/libxml2.so.2 \
+  --link=/usr/lib/libxslt.so.1 \
+  --link=/usr/lib/libexslt.so.0 \
+  --link=/lib/libz.so.1 \
   --link=/lib/libcrypto.so.0.9.8 \
   --link=/lib/libssl.so.0.9.8 \
   --addlist=biber.files \
   --cachedeps=scancache \
   --output=biber-linux_x86_64 \
   /usr/local/perl/bin/biber
-
-\rm -f /tmp/libz.so.1
-\rm -f /tmp/libxml2.so.2
-\rm -rf /tmp/libxslt.so.1
-\rm -rf /tmp/libexslt.so.0
