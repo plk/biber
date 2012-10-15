8bb0bd7b3332012a979ae48f0a34dfc87403ca64
Philip Kime <Philip@kime.org.uk>
2012-10-15 15:19:13 +0200

    Bumped .bcf version and added form/lang attributes to labelspec elements

--- lib/Biber/Constants.pm
+++ lib/Biber/Constants.pm
@@ -24,7 +24,7 @@ our @EXPORT = qw{
 
 # Version of biblatex control file which this release expects. Matched against version
 # passed in control file. Used when checking the .bcf
-our $BCF_VERSION = '2.1';
+our $BCF_VERSION = '2.2';
 # Format version of the .bbl. Used when writing the .bbl
 our $BBL_VERSION = '2.1';
 
