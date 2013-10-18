848ca84face08bdba8a6e8d95447b0ec71e56804
Philip Kime <Philip@kime.org.uk>
2013-10-18 22:42:29 +0200

    Readying for 1.8

--- lib/Biber/Config.pm
+++ lib/Biber/Config.pm
@@ -17,7 +17,7 @@ use Log::Log4perl::Layout::SimpleLayout;
 use Log::Log4perl::Layout::PatternLayout;
 
 our $VERSION = '1.8';
-our $BETA_VERSION = 1; # Is this a beta version?
+our $BETA_VERSION = 0; # Is this a beta version?
 
 our $logger  = Log::Log4perl::get_logger('main');
 our $screen  = Log::Log4perl::get_logger('screen');
