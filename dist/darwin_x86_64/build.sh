71ccda535190d120e5b9469896210ab09d939c7b
Philip Kime <Philip@kime.org.uk>
2017-11-06 16:19:59 +0100

    Making build scripts more aware of Unicode::Collate version upgrades

--- dist/darwin_x86_64/build.sh
+++ dist/darwin_x86_64/build.sh
@@ -8,7 +8,24 @@
 # constructed in the code so Par::Packer can't auto-detect them.
 # Same with some of the output modules.
 
-cp /opt/local/libexec/perl5.26/sitebin/biber /tmp/biber-darwin
+# Unicode::Collate is bundled with perl but is often updated and this is a critical module
+# for biber. There are some parts of this module which must be explicitly bundled by pp.
+# Unfortunately, updates to the module go into site_perl and we must bundle the right version
+# and so we check if there are any newer versions than came with the version of perl we are using
+# by looking to see if there is a site_perl directory for the module. If there is, we use that
+# version.
+
+declare -r perlv='5.26'
+declare ucpath="/opt/local/lib/perl5/${perlv}/Unicode/Collate"
+
+# Unicode::Collate has a site_perl version so has been updated since this
+# perl was released
+if [ -d "/opt/local/lib/perl5/site_perl/${perlv}/darwin-thread-multi-2level/Unicode/Collate" ]
+then
+  ucpath="/opt/local/lib/perl5/site_perl/${perlv}/darwin-thread-multi-2level/Unicode/Collate"
+fi
+
+cp /opt/local/libexec/perl${perlv}/sitebin/biber /tmp/biber-darwin
 
 PAR_VERBATIM=1 pp \
   --unicode \
@@ -35,7 +52,7 @@ PAR_VERBATIM=1 pp \
   --module=DateTime \
   --link=/opt/local/lib/libz.1.dylib \
   --link=/opt/local/lib/libiconv.2.dylib \
-  --link=/opt/local/libexec/perl5.26/sitebin/libbtparse.dylib \
+  --link=/opt/local/libexec/perl${perlv}/sitebin/libbtparse.dylib \
   --link=/opt/local/lib/libxml2.2.dylib \
   --link=/opt/local/lib/libxslt.1.dylib \
   --link=/opt/local/lib/libgdbm.4.dylib \
@@ -44,7 +61,20 @@ PAR_VERBATIM=1 pp \
   --link=/opt/local/lib/libcrypto.1.0.0.dylib \
   --link=/opt/local/lib/liblzma.5.dylib \
   --link=/opt/local/lib/libintl.8.dylib \
-  --addlist=biber.files \
+  --addfile="../../data/biber-tool.conf;lib/Biber/biber-tool.conf" \
+  --addfile="../../data/schemata/config.rnc;lib/Biber/config.rnc" \
+  --addfile="../../data/schemata/config.rng;lib/Biber/config.rng" \
+  --addfile="../../data/schemata/bcf.rnc;lib/Biber/bcf.rnc" \
+  --addfile="../../data/schemata/bcf.rng;lib/Biber/bcf.rng" \
+  --addfile="../../lib/Biber/LaTeX/recode_data.xml;lib/Biber/LaTeX/recode_data.xml" \
+  --addfile="../../data/bcf.xsl;lib/Biber/bcf.xsl" \
+  --addfile="${ucpath}/Locale;lib/Unicode/Collate/Locale" \
+  --addfile="${ucpath}/CJK;lib/Unicode/Collate/CJK;lib/Unicode/Collate/CJK" \
+  --addfile="${ucpath}/allkeys.txt;lib/Unicode/Collate/allkeys.txt" \
+  --addfile="${ucpath}/keys.txt;lib/Unicode/Collate/keys.txt" \
+  --addfile="/opt/local/lib/perl5/site_perl/${perlv}/Mozilla/CA/cacert.pem;lib/Mozilla/CA/cacert.pem" \
+  --addfile="/opt/local/lib/perl5/site_perl/${perlv}/Business/ISBN/RangeMessage.xml;lib/Business/ISBN/RangeMessage.xml" \
+  --addfile="/opt/local/lib/perl5/site_perl/${perlv}/darwin-thread-multi-2level/auto/Unicode/LineBreak/LineBreak.bundle;lib/auto/Unicode/LineBreak/LineBreak.bundle" \
   --cachedeps=scancache \
   --output=biber-darwin_x86_64 \
   /tmp/biber-darwin
