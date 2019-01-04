afa6c580bdf599e9aaacbb9df58ae9f468734b46
Philip Kime <Philip@kime.org.uk>
2019-01-04 21:34:19 +0100

    Increased copyright dates

--- lib/Biber/Output/base.pm
+++ lib/Biber/Output/base.pm
@@ -430,7 +430,7 @@ L<https://github.com/plk/biber/issues>.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009-2018 François Charette and Philip Kime, all rights reserved.
+Copyright 2009-2019 François Charette and Philip Kime, all rights reserved.
 
 This module is free software.  You can redistribute it and/or
 modify it under the terms of the Artistic License 2.0.
