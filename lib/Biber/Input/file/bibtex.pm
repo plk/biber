484ec35cb0003f0088bd7b763e8c19375c5e1d58
Philip Kime <Philip@kime.org.uk>
2020-03-24 20:10:33 +0100

    Fix for #309

--- lib/Biber/Input/file/bibtex.pm
+++ lib/Biber/Input/file/bibtex.pm
@@ -122,7 +122,10 @@ sub extract_entries {
   }
 
   # Check for files with no macros - they also confuse btparse
-  my $tbuf = File::Slurper::read_text($filename, $encoding);
+  my $tbuf;
+  unless (eval {$tbuf = File::Slurper::read_text($filename, $encoding)}) {
+    biber_error("Data file '$filename' cannot be read in encoding '$encoding': $@");
+  }
   unless ($tbuf =~ m/\@/) {
     biber_warn("Data source '$filename' contains no BibTeX entries/macros, ignoring");
     return @rkeys;
