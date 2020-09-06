1994587e37d194d877e3fd465dd5d49120a7e2be
Philip Kime <Philip@kime.org.uk>
2020-09-06 23:06:59 +0200

    Addressing #331

--- lib/Biber/Config.pm
+++ lib/Biber/Config.pm
@@ -615,6 +615,8 @@ If returns the first file found among:
 
 =item * C<$ENV{XDG_CONFIG_HOME}/biber/biber.conf>
 
+=item * C<$HOME/.config/biber/biber.conf>
+
 =item * C<$HOME/Library/biber/biber.conf> (Mac OSX only)
 
 =item * C<$ENV{APPDATA}/biber.conf> (Windows only)
@@ -639,6 +641,10 @@ sub config_file {
   elsif ( defined $ENV{XDG_CONFIG_HOME} and
     -f File::Spec->catfile($ENV{XDG_CONFIG_HOME}, "biber", $BIBER_CONF_NAME) ) {
     $biberconf = File::Spec->catfile($ENV{XDG_CONFIG_HOME}, "biber", $BIBER_CONF_NAME);
+  }
+ # See https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+  elsif ( -f File::Spec->catfile($ENV{HOME}, ".config", "biber", $BIBER_CONF_NAME) ) {
+    $biberconf = File::Spec->catfile($ENV{HOME}, ".config", "biber", $BIBER_CONF_NAME);
   }
   elsif ( $^O =~ /(?:Mac|darwin)/ and
     -f File::Spec->catfile($ENV{HOME}, "Library", "biber", $BIBER_CONF_NAME) ) {
