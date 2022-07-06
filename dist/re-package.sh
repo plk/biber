faf8382149433885dd0f7f884bc439fd723b2a61
plk <Philip@kime.org.uk>
2022-07-06 18:38:16 +0200

    Updating build script

--- dist/re-package.sh
+++ dist/re-package.sh
@@ -10,6 +10,7 @@ cd ${ROOT}
 declare VER=$1
 declare RELEASE="current"
 declare PLATFORMS=("linux_x86_64" "MSWIN64" "MSWIN32" "darwinlegacy_x86_64" "darwin_universal")
+declare METAPLATFORMS=("linux" "windows" "windows" "macos" "macos")
 declare SFPLATFORMS=("Linux" "Windows" "Windows" "MacOS" "MacOS")
 declare EXTS=("tar.gz" "zip" "zip" "tar.gz" "tar.gz")
 
@@ -23,20 +24,27 @@ EOF
 # Binaries
 for i in "${!PLATFORMS[@]}"; do
     PLATFORM=${PLATFORMS[i]}
+    METAPLATFORM=${METAPLATFORMS[i]}
     # CTAN requires top-level dir in lowercase
-    LCPLATFORM=$(echo "$PLATFORM" | tr '[:upper:]' '[:lower:]')
     SFPLATFORM=${SFPLATFORMS[i]}
     EXT=${EXTS[i]}
     if [ ! -e biber-$PLATFORM.tgz ]; then
-      echo -n "Packaging $PLATFORM ... "
-      mkdir biber-$LCPLATFORM 2>/dev/null
-      /opt/local/bin/wget --content-disposition --level=0 -c https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/$RELEASE/binaries/$SFPLATFORM/biber-$PLATFORM.$EXT -O biber-$LCPLATFORM/biber-$VER-$PLATFORM.$EXT >/dev/null 2>&1
+      echo -n "Retrieving $PLATFORM ... "
+      mkdir biber-$METAPLATFORM 2>/dev/null
+      /opt/local/bin/wget --content-disposition --level=0 -c https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/$RELEASE/binaries/$SFPLATFORM/biber-$PLATFORM.$EXT -O biber-$METAPLATFORM/biber-$VER-$PLATFORM.$EXT >/dev/null 2>&1
       [ $? -eq 0 ] || exit 1
-      create-readme $PLATFORM biber-$LCPLATFORM/README
-      tar zcf biber-$PLATFORM.tgz biber-$LCPLATFORM
-      \rm -rf biber-$LCPLATFORM
+      create-readme $PLATFORM biber-$METAPLATFORM/README
       echo "done"
-  fi
+    fi
+done
+
+SMETAPLATFORMS=($(printf "%s\n" "${METAPLATFORMS[@]}" | sort -u))
+for i in "${!SMETAPLATFORMS[@]}"; do
+  SMETAPLATFORM=${SMETAPLATFORMS[i]}
+  echo -n "Packaging $SMETAPLATFORM ... "
+  tar zcf biber-$SMETAPLATFORM.tgz biber-$SMETAPLATFORM
+  \rm -rf biber-$SMETAPLATFORM
+  echo "done"
 done
 
 # base
