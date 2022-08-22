a48836cc54dd78318bd3c97da32a0891afd78f0e
plk <Philip@kime.org.uk>
2022-08-22 22:00:15 +0200

    Generalising repack script

--- dist/re-package.sh
+++ dist/re-package.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Collect all binaries and re-package for CTAN
-# re-package.sh <biber version>
+# re-package.sh <biber version> <releasefolder>
 
 declare -r ROOT='/tmp/biber-repack'
 
 mkdir -p ${ROOT}
 cd ${ROOT}
 declare VER=$1
-declare RELEASE="current"
+declare RELEASE=${2:-"current"}
+declare PACKAGEEXT=""
 declare PLATFORMS=("linux_x86_32" "linux_x86_64" "MSWIN64" "MSWIN32" "darwinlegacy_x86_64" "darwin_universal")
 declare METAPLATFORMS=("linux" "linux" "windows" "windows" "macos" "macos")
 declare SFPLATFORMS=("Linux" "Linux" "Windows" "Windows" "MacOS" "MacOS")
@@ -30,12 +31,12 @@ for i in "${!PLATFORMS[@]}"; do
     # CTAN requires top-level dir in lowercase
     SFPLATFORM=${SFPLATFORMS[i]}
     EXT=${EXTS[i]}
-    if [ ! -e biber-$PLATFORM.tgz ]; then
+    if [ ! -e biber$PACKAGEEXT-$PLATFORM.tgz ]; then
       echo -n "Retrieving $PLATFORM ... "
-      mkdir biber-$METAPLATFORM 2>/dev/null
-      /opt/local/bin/wget --content-disposition --level=0 -c https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/$RELEASE/binaries/$SFPLATFORM/biber-$PLATFORM.$EXT -O biber-$METAPLATFORM/biber-$VER-$PLATFORM.$EXT >/dev/null 2>&1
+      mkdir biber$PACKAGEEXT-$METAPLATFORM 2>/dev/null
+      /opt/local/bin/wget --content-disposition --level=0 -c https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/$RELEASE/binaries/$SFPLATFORM/biber$PACKAGEEXT-$PLATFORM.$EXT -O biber$PACKAGEEXT-$METAPLATFORM/biber$PACKAGEEXT-$VER-$PLATFORM.$EXT >/dev/null 2>&1
       [ $? -eq 0 ] || exit 1
-      create-readme $METAPLATFORM biber-$METAPLATFORM/README
+      create-readme $METAPLATFORM biber$PACKAGEEXT-$METAPLATFORM/README
       echo "done"
     fi
 done
@@ -44,21 +45,21 @@ SMETAPLATFORMS=($(printf "%s\n" "${METAPLATFORMS[@]}" | sort -u))
 for i in "${!SMETAPLATFORMS[@]}"; do
   SMETAPLATFORM=${SMETAPLATFORMS[i]}
   echo -n "Packaging $SMETAPLATFORM ... "
-  tar zcf biber-$SMETAPLATFORM.tgz biber-$SMETAPLATFORM
-  \rm -rf biber-$SMETAPLATFORM
+  tar zcf biber$PACKAGEEXT-$SMETAPLATFORM.tgz biber-$SMETAPLATFORM
+  \rm -rf biber$PACKAGEEXT-$SMETAPLATFORM
   echo "done"
 done
 
 # base
-if [ ! -e biber-base.tgz ]; then
+if [ ! -e biber$PACKAGEEXT-base.tgz ]; then
   echo -n "Packaging base ... "
-  mkdir biber 2>/dev/null
-  cd biber
+  mkdir biber$PACKAGEEXT 2>/dev/null
+  cd biber$PACKAGEEXT
   mkdir source
   mkdir documentation
-  /opt/local/bin/wget --content-disposition --level=0 -c https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/$RELEASE/biblatex-biber.tar.gz -O source/biblatex-biber.tar.gz >/dev/null 2>&1
+  /opt/local/bin/wget --content-disposition --level=0 -c https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/$RELEASE/biblatex-biber.tar.gz -O source/biblatex-biber$PACKAGEEXT.tar.gz >/dev/null 2>&1
   [ $? -eq 0 ] || exit 1
-  /opt/local/bin/wget --content-disposition --level=0 -c https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/$RELEASE/documentation/biber.pdf -O documentation/biber.pdf >/dev/null 2>&1
+  /opt/local/bin/wget --content-disposition --level=0 -c https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/$RELEASE/documentation/biber.pdf -O documentation/biber$PACKAGEEXT.pdf >/dev/null 2>&1
   [ $? -eq 0 ] || exit 1
   /opt/local/bin/wget --content-disposition --level=0 -c https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/$RELEASE/Changes -O documentation/Changes >/dev/null 2>&1
   [ $? -eq 0 ] || exit 1
@@ -66,7 +67,7 @@ if [ ! -e biber-base.tgz ]; then
   [ $? -eq 0 ] || exit 1
   /opt/local/bin/wget --content-disposition --level=0 -c https://sourceforge.net/projects/biblatex-biber/files/biblatex-biber/README.md -O README.md >/dev/null 2>&1
   cd ..
-  tar zcf biber-base.tgz biber
-  \rm -rf biber
+  tar zcf biber$PACKAGEEXT-base.tgz biber
+  \rm -rf biber$PACKAGEEXT
   echo "done"
 fi
