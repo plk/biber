a91806d2b31df97efa8f5db5c5018b1e90d3710b
plk <Philip@kime.org.uk>
2023-01-02 10:46:43 +0100

    Updated copyright for 2023

--- lib/Biber/Output/bblxml.pm
+++ lib/Biber/Output/bblxml.pm
@@ -808,7 +808,7 @@ L<https://github.com/plk/biber/issues>.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2012-2022 Philip Kime, all rights reserved.
+Copyright 2012-2023 Philip Kime, all rights reserved.
 
 This module is free software.  You can redistribute it and/or
 modify it under the terms of the Artistic License 2.0.
