ffe84464ef33f3ddaba253e991e21acb465a8d1b
plk <Philip@kime.org.uk>
2023-03-05 22:23:24 +0100

    New DEV version

--- dist/empty_tree.sh
+++ dist/empty_tree.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# The paths sometimes change, check here: https://sourceforge.net/p/forge/documentation/SCP/
+# For interactive login, use ssh -t philkime@shell.sourceforge.net create
 
 if [ -z "$1" ]; then
   echo "No folder name provided!"
@@ -17,6 +19,8 @@ mkdir -p /tmp/sftree/binaries/ARM
 
 chmod -R 777 /tmp/sftree
 
-scp -r /tmp/sftree/* philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/biblatex-biber/biblatex-biber/$1
+# Need to use -O for legacy compat since scp is too new for the SF servers
+# https://unix.stackexchange.com/questions/730328/scp-requires-directory-of-same-name-to-exist-on-target-server
+scp -O -r /tmp/sftree/* philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/biblatex-biber/$1/
 
 \rm -rf /tmp/sftree
