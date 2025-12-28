7d6abbfdad24fa71b6ad7e6c5c3200acf1b0e4cd
plk <Philip@kime.org.uk>
2025-12-28 21:04:03 +0100

    Updated for 2026

--- dist/build.sh
+++ dist/build.sh
@@ -181,23 +181,6 @@ if [ ! -e $DIR/biber-linux_x86_64.tar.gz ]; then
   cd $BASE
 fi
 
-# Build farm Linux MUSL 64-bit (Alpine)
-# if [ ! -e $DIR/biber-linux-musl_x86_64.tar.gz ]; then
-#   vmon musll64
-#   sleep 10
-#   ssh philkime@bbf-musll64 "sudo ntpdate ch.pool.ntp.org;cd biblatex-biber;git checkout $BRANCH;git pull;/usr/local/perl/bin/perl ./Build.PL;sudo ./Build installdeps;sudo ./Build install;cd dist/linux-musl_x86_64;$SCANCACHE./build.sh;cd ~/biblatex-biber;sudo ./Build realclean"
-#   scp philkime@bbf-musll64:biblatex-biber/dist/linux-musl_x86_64/biber-linux-musl_x86_64 $DIR/
-#   ssh philkime@bbf-musll64 "\\rm -f biblatex-biber/dist/linux-musl_x86_64/biber-linux-musl_x86_64"
-#   vmoff musll64
-#   cd $DIR
-#   mv biber-linux-musl_x86_64 $BINARYNAME
-#   chmod +x $BINARYNAME
-#   tar cf biber-linux-musl_x86_64.tar $BINARYNAME
-#   gzip biber-linux-musl_x86_64.tar
-#   \rm $BINARYNAME
-#   cd $BASE
-# fi
-
 # Stop here if JUSTBUILD is set
 if [ "$JUSTBUILD" = "1" ]; then
   echo "JUSTBUILD is set, will not upload anything";
@@ -234,11 +217,6 @@ fi
 if [ -e $DIR/biber-linux_x86_64.tar.gz ]; then
   scp biber-linux_x86_64.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/biblatex-biber/biblatex-biber/$RELEASE/binaries/Linux/biber-linux_x86_64.tar.gz
 fi
-
-# Linux MUSL 64-bit
-# if [ -e $DIR/biber-linux-musl_x86_64.tar.gz ]; then
-#   scp biber-linux-musl_x86_64.tar.gz philkime,biblatex-biber@frs.sourceforge.net:/home/frs/project/biblatex-biber/biblatex-biber/$RELEASE/binaries/Linux/biber-linux-musl_x86_64.tar.gz
-# fi
 
 # Doc
 cd $DOCDIR
